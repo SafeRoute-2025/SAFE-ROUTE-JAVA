1e2c9b1941842932167a2c2be45375ccd6eef74d
Mauricio Pereira <114964115+Mauricio-Pereira@users.noreply.github.com>
2025-05-27 15:01:57 -0300

    UPDATING

---
+++ src/main/java/com/fiap/safe_route/Gs2025Application.java
@@ -0,0 +1,13 @@
+package com.fiap.safe_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs2025Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/fiap/safe_route/model/User.java
@@ -0,0 +1,43 @@
+package com.fiap.safe_route.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "GS2025_USUARIO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "SEQ_GS2025_USUARIO", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_CADASTRO")
+    private Date createdAt;
+}
