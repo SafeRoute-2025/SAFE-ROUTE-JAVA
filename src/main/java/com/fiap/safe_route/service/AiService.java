a09fdca9e882d8e4602a9812c1535ef8d708ebd6
Mauricio Pereira <114964115+Mauricio-Pereira@users.noreply.github.com>
2025-06-02 19:46:55 -0300

    FEAT: implementing spring AI

--- src/main/java/com/fiap/safe_route/service/AiService.java
+++ src/main/java/com/fiap/safe_route/service/AiService.java
@@ -2,6 +2,7 @@ package com.fiap.safe_route.service;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -11,15 +12,15 @@ public class AiService {
 
     private final ChatLanguageModel model;
 
-    public AiService() {
+    public AiService(@Value("${spring.ai.ollama.base-url}") String baseUrl){
         this.model = OllamaChatModel.builder()
-                .baseUrl("http://localhost:11434")
+                .baseUrl(baseUrl)
                 .modelName("tinyllama")
                 .build();
     }
     public String gerarDicaSeguranca(Locale locale) {
         String prompt = locale.getLanguage().equals("pt")
-                ? "Me dê uma dica rápida de segurança em situações de desastres naturais"
+                ? "Give me a quick safety tip for natural disaster situations"
                 : "Give me a quick safety tip for natural disaster situations";
 
         return model.generate(prompt);
